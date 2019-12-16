73de0ac14a227133526950aed0670a9f8e19a7f9
firas haj yedder <firasbenhajyedder@hotmail.com>
2019-12-16 16:55:52 +0100

    4

--- app/src/main/java/api/UtilisateurHelper.java
+++ app/src/main/java/api/UtilisateurHelper.java
@@ -1,9 +1,14 @@
 package api;
 
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
 import modules.Utilisateur;
 
 public class UtilisateurHelper
